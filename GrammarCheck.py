4f36fadb6d8bd8fbaa3980b3e6d5f456badb5018
Jagarnath123 <njagarnathservicenow@gmail.com>
2023-05-07 09:23:27 +0000

    Initial Commit

--- GrammarCheck.py
+++ GrammarCheck.py
@@ -1,8 +1,12 @@
 # Fix Spelling and Grammar with Python
 # pip install gingerit
 from gingerit.gingerit import GingerIt
-def Fix_Grammar(text):
+
+
+def fix_grammar(text):
     ginger = GingerIt()
     correction = ginger.parse(text)
     return correction
-Fix_Grammer("The fliwers is very good")
+
+
+fix_grammar("he fliwers is very good")
